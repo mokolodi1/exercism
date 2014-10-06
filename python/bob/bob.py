3a3c855ef48e4028d9e29da771c5b308985ca6c4
mokolodi1 <mokolodi1@gmail.com>
2014-10-05 20:59:13 -0400

    first commit

---
+++ python/bob/bob.py
@@ -0,0 +1,10 @@
+def hey_mine(what):
+    what = what.strip()
+    if what == '':
+        return 'Fine. Be that way!'
+    if all(all(not char.islower() for char in word) for word in what.split()):
+        if any(any(char.isalpha() for char in word) for word in what.split()):
+            return 'Whoa, chill out!'
+    if what[-1] == '?':
+        return 'Sure.'
+    return 'Whatever.'
