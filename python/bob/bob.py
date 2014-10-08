f008c4480042a9f72fb77c136366338509255044
mokolodi1 <mokolodi1@gmail.com>
2014-10-07 23:42:23 -0400

    fixed bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,10 +1,9 @@
-def hey_mine(what):
+def hey(what):
     what = what.strip()
     if what == '':
         return 'Fine. Be that way!'
-    if all(all(not char.islower() for char in word) for word in what.split()):
-        if any(any(char.isalpha() for char in word) for word in what.split()):
-            return 'Whoa, chill out!'
+    if what.isupper():
+        return 'Whoa, chill out!'
     if what[-1] == '?':
         return 'Sure.'
     return 'Whatever.'
