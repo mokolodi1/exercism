23c7a1430e6bf39fbe5bb42d9e163e4f32a336da
mokolodi1 <mokolodi1@gmail.com>
2014-10-07 23:33:10 -0400

    fixed difference-of-squares using nitpicks

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,8 +1,9 @@
+# (num ** 2 + num) / 2 === sum(range(num + 1))
 def square_of_sum(num):
-    return sum(range(num + 1)) ** 2
+    return ((num ** 2 + num) / 2) ** 2
 
 def sum_of_squares(num):
     return sum([i ** 2 for i in range(num + 1)])
 
 def difference(num):
-    return abs(sum_of_squares(num) - square_of_sum(num))
+    return square_of_sum(num) - sum_of_squares(num)
