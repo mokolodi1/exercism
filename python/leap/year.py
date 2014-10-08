ac20c1381d49af3ab3f955dbff74864a92925ed0
mokolodi1 <mokolodi1@gmail.com>
2014-10-07 23:37:19 -0400

    fixed year.py

--- python/leap/year.py
+++ python/leap/year.py
@@ -1,11 +1,5 @@
-#
-# Is it a leap year?
-#
 def is_leap_year(year):
     if year % 100 == 0:
-        if year % 400 == 0:
-            return True
-        else:
-            return False
+        return year % 400 == 0
     return year % 4 == 0
     
