1724db102483fd225fad6812a8713ddeb8200554
mokolodi1 <mokolodi1@gmail.com>
2014-10-11 12:57:03 -0400

    fixed dna.py

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,5 +1,6 @@
 from string import maketrans
 
-def to_rna(string):
-    return string.translate(maketrans('GCTA', 'CGAU'))
+translation = maketrans('GCTA', 'CGAU')
 
+def to_rna(string):
+    return string.translate(translation)
